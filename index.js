542496ff1b1597e4084d981d10db782fc367df1e
dlfdyd96 <1yongs_@naver.com>
2020-02-18 22:36:15 +0900

    #2.4 Handling Routes with Express

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ function handleListening() {
     console.log(`listening on : http://localhost:${PORT}`)
 }
 
+function handleHome(req, res){
+    console.log(req)
+    res.send('hello from home!!')
+    console.log('hi from home!!')
+}
+
+function handleProfile(req,res) {
+    res.send('you are on my profile');
+}
+
+app.get("/", handleHome)
+app.get("/profile", handleProfile)
+
 app.listen(4000, handleListening);
