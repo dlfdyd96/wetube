7f4ad27d1744ca535c656f91b44607d5e43a2284
dlfdyd96 <1yongs_@naver.com>
2020-02-23 18:56:08 +0900

    #2.7 Express Core: Middlewares part Two (9:22)

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-import "core-js"
+import "core-js";
 import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`listening on : http://localhost:${PORT}`)
+const handleListening = () => console.log(`listening on : http://localhost:${PORT}`);
 
-const handleHome = (req, res) => res.send('hello from home!!')
+const handleHome = (req, res) => res.send('hello from home!!');
 
 const handleProfile = (req, res) => res.send('you are on my profile');
 
-const betweenHome = (req, res, next) => {
-    console.log('im between');
-    next();
-}
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 
-app.use(betweenHome);
+app.get("/", handleHome);
 
-app.get("/", betweenHome, handleHome)
-
-app.get("/profile", handleProfile)
+app.get("/profile", handleProfile);
 
 app.listen(4000, handleListening);
