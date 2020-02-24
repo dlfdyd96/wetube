adb170835b5983f949fb5787b292397bc74e9785
dlfdyd96 <1yongs_@naver.com>
2020-02-24 17:47:00 +0900

    #2.14 Layouts with Pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,8 @@
 export const homeController = (req,res) => res.render("home");
-export const searchController = (req,res) => res.send("Search");
+export const searchController = (req,res) => res.render("Search");
 
-export const videosController = (req,res) => res.send("videos");
-export const uploadController = (req,res) => res.send("upload");
-export const videosDetailController = (req,res) => res.send("videosDetail");
-export const editVideoController = (req,res) => res.send("editVideo");
-export const deleteVideoController = (req,res) => res.send("deleteVideo");
+export const videosController = (req,res) => res.render("videos");
+export const uploadController = (req,res) => res.render("upload");
+export const videosDetailController = (req,res) => res.render("videosDetail");
+export const editVideoController = (req,res) => res.render("editVideo");
+export const deleteVideoController = (req,res) => res.render("deleteVideo");
