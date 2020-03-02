2f5af45c3ff5fc8856b5a806d6b24acebc9f29b0
dlfdyd96 <1yongs_@naver.com>
2020-03-02 13:02:34 +0900

    #3.3 Video Model

--- init.js
+++ init.js
@@ -1,8 +1,10 @@
 import "core-js";
 import "./db"
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config()
 
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;  //만일 못찾으면 4000번으로
 
 const handleListening = () => console.log(`✔Listening on : http://localhost:${PORT}`)
 
